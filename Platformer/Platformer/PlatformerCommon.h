70e1f2a2b514590e83f431add36fd2d2c4cd8cdf
JackCactus <jackhookham@gmail.com>
2017-03-14 17:14:34 +0000

    Common header

---
+++ Platformer/Platformer/PlatformerCommon.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_ttf.h>/*
+#include "PhysicsManager.h"
+#include "GraphicsManager.h"*/
+
+extern TTF_Font* gFont;
+
+//extern PhysicsManager* gPhysicsManager;
+//extern GraphicsManager* gGraphicsManager;
