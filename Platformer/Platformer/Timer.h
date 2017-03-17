137da48dcd6742c52befc86f4cb8b38dd78b33a8
JackCactus <jackhookham@gmail.com>
2017-03-17 13:50:03 +0000

    Timer class

--- Platformer/Platformer/Timer.h
+++ Platformer/Platformer/Timer.h
@@ -1,8 +1,34 @@
 #pragma once
+#include <SDL.h>
+
 class Timer
 {
 public:
 	Timer();
 	~Timer();
+
+	//Clock actions
+	void start();
+	void stop();
+	void pause();
+	void unpause();
+
+	//Get the timer's time
+	Uint32 getTicks();
+
+	//Check the status of the timer
+	bool isStarted();
+	bool isPaused();
+
+private:
+	//The clock time when the timer started
+	Uint32 mStartTicks;
+
+	//The ticks stored when the timer was paused
+	Uint32 mPausedTicks;
+
+	//The timer status
+	bool mPaused;
+	bool mStarted;
 };
 
